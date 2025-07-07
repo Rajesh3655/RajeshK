6f38e5c79a675789ec7a506de6c79f3ef3981fbb
Rajesh3655 <138114625+Rajesh3655@users.noreply.github.com>
2025-07-07 10:25:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request, redirect, flash, url_for, jso
 import smtplib
 from email.message import EmailMessage
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'rajesh-portfolio-secret-key')
